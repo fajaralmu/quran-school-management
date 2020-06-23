49cab42e410e0d9fa9897dfe837e8b089e2ec2a0
Fajar AM <somabangsa@gmail.com>
2020-06-23 23:25:38 +0700

    add handler interceptor

---
+++ src/main/java/com/fajar/schoolmanagement/config/MyHandlerInterceptor.java
@@ -0,0 +1,43 @@
+package com.fajar.schoolmanagement.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * this class is registered in the xml configuration
+ * @author Republic Of Gamers
+ *
+ */
+@Slf4j
+public class MyHandlerInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		 
+		log.info("[preHandle][" + request + "]" + "[" + request.getMethod()  + "]" );
+		return super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		 
+		 log.info("[postHandle][" + request + "], viewName: {}", modelAndView.getViewName());
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+		super.afterCompletion(request, response, handler, ex);
+	}
+	
+
+}
