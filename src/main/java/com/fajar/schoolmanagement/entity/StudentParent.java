a45ad2c947ce1af93348d29f4457f4a1bd34ab4a
Fajar AM <somabangsa@gmail.com>
2020-07-03 06:47:59 +0700

    update parent form

--- src/main/java/com/fajar/schoolmanagement/entity/StudentParent.java
+++ src/main/java/com/fajar/schoolmanagement/entity/StudentParent.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fajar.schoolmanagement.annotation.AdditionalQuestionField;
 import com.fajar.schoolmanagement.annotation.Dto;
 import com.fajar.schoolmanagement.annotation.FormField;
 import com.fajar.schoolmanagement.dto.FieldType;
@@ -15,7 +16,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Dto("Wali Siswa")
+@Dto(value = "Wali Siswa", quistionare = true)
 @Entity
 @Table (name="student_parent")
 @Data
@@ -28,52 +29,68 @@ public class StudentParent extends BaseEntity{
 	 * 
 	 */
 	private static final long serialVersionUID = -5455451366947342120L;
+	static final String DATA_AYAH = "Identitas Ayah";
+	static final String DATA_IBU = "Identitas Ibu";
+	static final String DATA_ALL = "Lain-lain";
+	
 	@FormField
 	@Column(name="father_name")
+	@AdditionalQuestionField(DATA_AYAH)
 	private String fatherName;
 	
 	@FormField
-	@Column(name="mother_name")
-	private String motherName;
-	
-	@FormField
 	@Column(name="father_birth_place")
-	private String fatherPlaceOfBirth;
-	
-	@FormField
-	@Column(name="mother_birth_place")
-	private String motherPlaceOfBirth;
+	@AdditionalQuestionField(DATA_AYAH)
+	private String fatherPlaceOfBirth; 
 	
 	@FormField
 	@Column(name="father_date_of_birth")
+	@AdditionalQuestionField(DATA_AYAH)
 	private Date fatherDateOfBirth;
 	
 	@FormField
-	@Column(name="mother_date_of_birth")
-	private Date motherDateOfBirth;
-	
-	@FormField
 	@Column(name="father_education")
-	private String fatherEducation;
-	
-	@FormField
-	@Column(name="mother_education")
-	private String motherEducation;
-	
+	@AdditionalQuestionField(DATA_AYAH)
+	private String fatherEducation; 
+
 	@FormField
 	@Column(name="father_occupation")
+	@AdditionalQuestionField(DATA_AYAH)
 	private String fatherOccupation;
 	
 	@FormField
+	@Column(name="mother_name")
+	@AdditionalQuestionField(DATA_IBU)
+	private String motherName; 
+	
+	@FormField
+	@Column(name="mother_birth_place")
+	@AdditionalQuestionField(DATA_IBU)
+	private String motherPlaceOfBirth; 
+	
+	@FormField
+	@Column(name="mother_date_of_birth")
+	@AdditionalQuestionField(DATA_IBU)
+	private Date motherDateOfBirth; 
+	
+	@FormField
+	@Column(name="mother_education")
+	@AdditionalQuestionField(DATA_IBU)
+	private String motherEducation; 
+	
+	@FormField
 	@Column(name="mother_occupation")
+	@AdditionalQuestionField(DATA_IBU)
 	private String motherOccupation;
 	
 	@FormField(type = FieldType.FIELD_TYPE_TEXTAREA)
 	@Column(name="address")
+	@AdditionalQuestionField(DATA_ALL)
 	private String address;
 	
 	@FormField
 	@Column(name="phone_number")
+	@AdditionalQuestionField(DATA_ALL)
 	private String phoneNumber;
 
 }
--- src/main/webapp/WEB-INF/jsp/school/login-page.jsp
+++ src/main/webapp/WEB-INF/jsp/school/login-page.jsp
@@ -44,7 +44,7 @@
 </script>
 <div class="content">
 	<p id="info" align="center"></p>
-	<div class="card" style="max-width: 200px; margin: auto">
+	<div class="card" style="max-width: 400px; margin: auto">
 		<div class="card-header">Please Login</div>
 		<div class="card-body">
 			<div class="login-form">
